58fb164f49444f5688652b4b9a98139548e53e92
RosyGraph <chris.stevenson@live.com>
2023-07-14 22:53:11 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Get a seal of approval! Photos provided by Pexels.
+
+![seal](https://images.pexels.com/photos/185032/pexels-photo-185032.jpeg?auto=compress&cs=tinysrgb&h=130)
+
+Seal by Barthy Bonhomme
---
+++ seal.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import os
+from pathlib import Path as P
+from random import choice
+from time import sleep
+
+import requests
+from dotenv import load_dotenv
+from pyperclip import copy
+
+load_dotenv()
+
+PEXELS_API_KEY = os.getenv("PEXELS_API_KEY", "")
+PEXELS_BASE_URL = "https://api.pexels.com/v1/search"
+
+
+def get_seal():
+    if not PEXELS_API_KEY:
+        raise ValueError("No PEXELS_API_KEY provided")
+    headers = {"Authorization": PEXELS_API_KEY}
+    params = {"query": "seal", "per_page": 80}
+    response = requests.get(PEXELS_BASE_URL, headers=headers, params=params)
+    response.raise_for_status()
+    data = response.json()
+    random_seal = choice(data["photos"])
+    url_to_seal = random_seal["src"]["original"]
+    author = random_seal["photographer"]
+    return f"![seal]({url_to_seal})", author
+
+
+def scrape_seal_urls():
+    if not PEXELS_API_KEY:
+        raise ValueError("No PEXELS_API_KEY provided")
+    headers = {"Authorization": PEXELS_API_KEY}
+    params = {"query": "seal", "per_page": 80}
+    response = requests.get(PEXELS_BASE_URL, headers=headers, params=params)
+    if response.status_code == 429:
+        print("Rate limit reached, sleeping 1 hour...")
+        sleep(60 * 60)
+        response = requests.get(PEXELS_BASE_URL, headers=headers, params=params)
+    data = response.json()
+    seal_ids = [seal["id"] for seal in data["photos"]]
+    while next_page := data.get("next_page"):
+        params["page"] = next_page
+        response = requests.get(PEXELS_BASE_URL, headers=headers, params=params)
+        response.raise_for_status()
+        data = response.json()
+        seal_ids.extend([seal["id"] for seal in data["photos"]])
+        if "X-Ratelimit-Remaining" in response.headers:
+            rate_limit_remaining = int(response.headers["X-Ratelimit-Remaining"])
+            if rate_limit_remaining < 80:
+                print("Rate limit reached, sleeping 1 hour...")
+    return seal_ids
+
+
+def main():
+    seal, author = get_seal()
+    copy(seal)
+    print("Copied seal to clipboard!")
+    print(f"Seal by {author}")
+
+
+if __name__ == "__main__":
+    scraped_ids = scrape_seal_urls()
+    print("\n".join(scraped_ids))
+    P("seal_ids.txt").write_text("\n".join(scrape_seal_urls()))
