30df493438f932c4cf9303d8ec01d74f18cebace
RosyGraph <chris.stevenson@live.com>
2023-07-21 11:50:05 -0600

    Use medium instead of original picture size

--- seal.py
+++ seal.py
@@ -23,7 +23,7 @@ def get_seal():
     response.raise_for_status()
     data = response.json()
     random_seal = choice(data["photos"])
-    url_to_seal = random_seal["src"]["original"]
+    url_to_seal = random_seal["src"]["medium"]
     author = random_seal["photographer"]
     return f"![seal]({url_to_seal})", author
 
